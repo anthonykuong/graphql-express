fdc5fa9811f6c8f6b8dd560a41ecaf56747a275a
Anthony Ku Ong <anthonykuong@gmail.com>
2021-01-24 22:09:17 -0500

    fifth mock
    m6
    
    M7

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,10 +5,11 @@ const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 // dummy data
 var books = [
-    { name: 'M3 Name of the Wind', genre: 'Fantasy', id: '1' },
-    { name: 'M3 The Final Empire', genre: 'Fantasy', id: '2' },
-    { name: 'M3 The Long Earth', genre: 'Sci-Fi', id: '3' },
-    { name: 'M3 The Long Earth', genre: 'Sci-Fi', id: '4' },
+    { name: 'M7 Name of the Wind', genre: 'Fantasy', id: '1' },
+    { name: 'M7 The Final Empire', genre: 'Fantasy', id: '2' },
+    { name: 'M7 The Long Earth', genre: 'Sci-Fi', id: '3' },
+    { name: 'M7 The Long Earth', genre: 'Sci-Fi', id: '4' },
+    { name: 'M7 The Medium Earth', genre: 'Sci-Fi', id: '5'},
 ];
 
 const BookType = new GraphQLObjectType({
